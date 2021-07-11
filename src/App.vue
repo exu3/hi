447f855026b4d9e41a13a9497c6997ecc48b7888
eilla1 <git@ella.cx>
2021-07-11 10:57:28 -0700

    :tada: init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <HelloWorld msg="Hi." />
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 4em;
+}
+</style>
